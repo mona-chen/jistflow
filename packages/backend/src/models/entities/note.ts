2efdbae42e75992965069f6e7601a724cc5da9a1
Laura Hausmann <laura@hausmann.dev>
2023-09-28 02:42:14 +0200

    Add index to improve user/notes endpoint performance

--- packages/backend/src/models/entities/note.ts
+++ packages/backend/src/models/entities/note.ts
@@ -16,6 +16,7 @@ import { Channel } from "./channel.js";
 @Index("IDX_NOTE_TAGS", { synchronize: false })
 @Index("IDX_NOTE_MENTIONS", { synchronize: false })
 @Index("IDX_NOTE_VISIBLE_USER_IDS", { synchronize: false })
+@Index("IDX_note_userId_id", ["userId", "id"])
 export class Note {
 	@PrimaryColumn(id())
 	public id: string;
