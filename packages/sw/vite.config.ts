5033d4e6825107dcd7f5c1d41d40f7e58dbac796
ThatOneCalculator <kainoa@t1c.dev>
2023-10-08 18:40:48 -0700

    build: :bug: sw firefish-js imports

--- packages/sw/vite.config.ts
+++ packages/sw/vite.config.ts
@@ -8,14 +8,25 @@ import viteCompression from "vite-plugin-compression";
 export default defineConfig({
 	mode: isProduction ? "production" : "development",
 	build: {
-		target: "modules",
-		outDir: "../../built/_sw_dist_",
-		assetsDir: "",
+		target: ["chrome87", "firefox78", "safari14", "es2017"],
+		manifest: "manifest.json",
 		rollupOptions: {
-			input: "./src/sw.ts",
+			input: {
+				app: "./src/sw.ts",
+			},
 			output: {
 				entryFileNames: "sw.js",
 			},
+		},
+		cssCodeSplit: true,
+		assetsInlineLimit: 0,
+		outDir: `${__dirname}/../../built/_sw_dist_`,
+		assetsDir: ".",
+		emptyOutDir: false,
+		sourcemap: process.env.NODE_ENV === "development",
+		reportCompressedSize: false,
+		commonjsOptions: {
+			include: [/firefish-js/, /node_modules/],
 		},
 	},
 	resolve: {
