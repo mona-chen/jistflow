70503d698f02963048c0205490b8f37e64167f4c
naskya <m@naskya.net>
2023-10-31 14:00:54 +0900

    fix: reactions_not_public condition

--- packages/backend/src/server/api/endpoints/users/reactions.ts
+++ packages/backend/src/server/api/endpoints/users/reactions.ts
@@ -49,7 +49,7 @@ export const paramDef = {
 export default define(meta, paramDef, async (ps, me) => {
 	const profile = await UserProfiles.findOneByOrFail({ userId: ps.userId });
 
-	if (me.id !== ps.userId && !profile.publicReactions) {
+	if (!profile.publicReactions && (me == null || me.id !== ps.userId)) {
 		throw new ApiError(meta.errors.reactionsNotPublic);
 	}
 
