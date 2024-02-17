099ba9ce65486d176ff6c7a2f22a137046834037
Laura Hausmann <laura@hausmann.dev>
2024-02-17 15:55:48 +0100

    [backend] Enforce JSON-LD profile when fetching activities

--- packages/backend/src/misc/fetch.ts
+++ packages/backend/src/misc/fetch.ts
@@ -48,7 +48,9 @@ export async function getJsonActivity(
 	});
 
 	const contentType = res.headers.get('content-type');
-	if (contentType == null || (contentType !== 'application/activity+json' && !contentType.startsWith('application/activity+json;') && contentType !== 'application/ld+json' && !contentType.startsWith('application/ld+json;')))
+	if (contentType == null ||
+		(contentType !== 'application/activity+json' && !contentType.startsWith('application/activity+json;') &&
+		(!contentType.startsWith('application/ld+json;') || !contentType.includes('profile="https://www.w3.org/ns/activitystreams"'))))
 		throw new Error(`getJsonActivity response had unexpected content-type: ${contentType}`);
 
 	return {
